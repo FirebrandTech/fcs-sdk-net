a71ef8e6c8d12e52d85eddc379ff92ebee9ad4f0
Andy Day <andy@firebrandtech.com>
2015-07-30 20:51:02 -0700

    add type to catalogcategory

--- Src/Model/Catalogs.cs
+++ Src/Model/Catalogs.cs
@@ -39,6 +39,7 @@ namespace Fcs.Model {
     public class CatalogCategory : IReturn<CatalogCategory> {
         public Guid? Id { get; set; }
         public string Visibility { get; set; }
+        public string Type { get; set; }
         public string Name { get; set; }
         public string Label { get; set; }
         public string Description { get; set; }
